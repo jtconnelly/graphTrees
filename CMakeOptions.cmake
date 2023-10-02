c95b1c8e5063d4b22d4150bd3231d481ac768541
James Desktop <jtc20659@gmail.com>
2023-10-01 20:35:46 -0400

    Setup CMake and Structure

---
+++ CMakeOptions.cmake
@@ -0,0 +1,3 @@
+option(BUILD_TEST_EXE "Add the Executable Project that will test the individual libraries" ON)
+option(INCLUDE_GRAPHS "Include the library that defines graphs and searching algorithms" ON)
+option(INCLUDE_TREES "Include the library that defines different sets of trees" ON)
